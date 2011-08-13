70bf1967d0a3be875665ad02b3198bacb8974985
Scott Lawrence <slawrence@gmail.com>
2011-08-12 20:26:18 -0400

    Interfaces for a potential interview sample coding exercise.

---
+++ ClaimsRUs/ClaimsRUs.Domain/IClaim.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ClaimsRUs.Domain
+{
+    public interface IClaim
+    {
+        IClaimant Claimant { get; set; }
+        IList<IClaimTag> GetTags();
+        void ApplyTag(IClaimTag claimTag);
+        bool HasClaimTag(IClaimTag claimTag);
+    }
+}
---
+++ ClaimsRUs/ClaimsRUs.Domain/IClaimant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClaimsRUs.Domain
+{
+    public interface IClaimant
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime DateOfBirth { get; set; }
+        int Age { get; }
+        string Street { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+        string SSN { get; set; }
+    }
+}
