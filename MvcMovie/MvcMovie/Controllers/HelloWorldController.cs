529ecf2f19c742667d65e3258effb049c06d6192
Scott Lawrence <slawrence@gmail.com>
2012-03-29 13:06:39 -0400

    MVC Movie tutorial (http://www.asp.net/mvc/tutorials/getting-started-with-aspnet-mvc3/getting-started-with-mvc3-part5-cs).
    Only the first five parts are complete.

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = string.Format("Hello {0}", name);
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,104 @@
+﻿using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{ 
+    public class MoviesController : Controller
+    {
+        private readonly MovieDbContext _db = new MovieDbContext();
+
+        //
+        // GET: /Movies/
+
+        public ViewResult Index()
+        {
+            return View(_db.Movies.ToList());
+        }
+
+        //
+        // GET: /Movies/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Movie movie = _db.Movies.Find(id);
+            return View(movie);
+        }
+
+        //
+        // GET: /Movies/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Movies/Create
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Movies.Add(movie);
+                _db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(movie);
+        }
+        
+        //
+        // GET: /Movies/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Movie movie = _db.Movies.Find(id);
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(movie).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        //
+        // GET: /Movies/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Movie movie = _db.Movies.Find(id);
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Movie movie = _db.Movies.Find(id);
+            _db.Movies.Remove(movie);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
